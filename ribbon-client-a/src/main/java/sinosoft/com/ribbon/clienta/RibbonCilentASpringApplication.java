87625de6d6e14a770541c2c10bfd771384a1c6d4
wangzhil <sinosoft>
2019-05-19 11:17:59 +0800

    skywalking 5.0.0-RC2 和 elastic 5.0.0 集成 <简易版>

--- ribbon-client-a/src/main/java/sinosoft/com/ribbon/clienta/RibbonCilentASpringApplication.java
+++ ribbon-client-a/src/main/java/sinosoft/com/ribbon/clienta/RibbonCilentASpringApplication.java
@@ -1,5 +1,8 @@
 package sinosoft.com.ribbon.clienta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -9,14 +12,12 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RoundRobinRule;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
-public class RibbonCilentASpringApplication {
+public class RibbonCilentASpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonCilentASpringApplication.class, args);
@@ -27,9 +28,20 @@ public class RibbonCilentASpringApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-	
-	@Bean
-	public IRule MyRule(){
-		return new RoundRobinRule ();
+
+	@Override
+	public void run(String... args) throws Exception {
+		Logger logger = LoggerFactory.getLogger(RibbonCilentASpringApplication.class);
+        logger.info("测试log");
+
+        for (int i = 0; i < 10; i++) {
+            logger.error("something wrong. id={}; name=Ryan-{};", i, i);
+        }
+		
 	}
+	
+//	@Bean
+//	public IRule MyRule(){
+//		return new RoundRobinRule ();
+//	}
 }
