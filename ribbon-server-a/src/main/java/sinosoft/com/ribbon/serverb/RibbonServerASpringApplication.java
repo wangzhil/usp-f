9f2ad0ce4a98ffff12e4957c90dab43c15aa498e
wangzhil <sinosoft>
2019-08-23 16:18:52 +0800

    rabbitmq

--- ribbon-server-a/src/main/java/sinosoft/com/ribbon/serverb/RibbonServerASpringApplication.java
+++ ribbon-server-a/src/main/java/sinosoft/com/ribbon/serverb/RibbonServerASpringApplication.java
@@ -1,16 +1,33 @@
 package sinosoft.com.ribbon.serverb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
-public class RibbonServerASpringApplication {
+public class RibbonServerASpringApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonServerASpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Logger logger = LoggerFactory.getLogger(RibbonServerASpringApplication.class);
+        
+        for (int i = 0; i < 10; i++) {
+        	logger.info(i+":测试log");
+        }
+        for (int i = 0; i < 100; i++) {
+            logger.error("something wrong. id={}; name=Ryan-{};", i, i);
+        }
+		
 	}
 }
