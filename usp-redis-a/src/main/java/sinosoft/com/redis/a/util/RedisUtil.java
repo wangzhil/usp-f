bc9cbc2e73ef052bd9142c782336a932bce516cb
wangzhil <developerwang@foxmail.com>
2020-02-12 18:07:07 +0800

    测试 contribution

--- usp-redis-a/src/main/java/sinosoft/com/redis/a/util/RedisUtil.java
+++ usp-redis-a/src/main/java/sinosoft/com/redis/a/util/RedisUtil.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
